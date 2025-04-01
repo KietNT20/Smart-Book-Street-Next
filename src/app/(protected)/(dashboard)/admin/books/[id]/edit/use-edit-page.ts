fefbdeafaa5e80aea87960ac309a54ef3a9e294c
nguyentuankiet <ntk28042002@gmail.com>
2025-04-01 22:07:39 +0700

    refactor: rename useBookSearchById to useGetBookByID in edit page hook

--- src/app/(protected)/(dashboard)/admin/books/[id]/edit/use-edit-page.ts
+++ src/app/(protected)/(dashboard)/admin/books/[id]/edit/use-edit-page.ts
@@ -1,4 +1,4 @@
-import { useBookSearchById } from '@/hooks/use-book-search';
+import { useGetBookByID } from '@/hooks/use-book-search';
 import { useBookMutations } from '@/hooks/use-books';
 import useDebounce from '@/hooks/use-debounce';
 import { useParams, useRouter } from 'next/navigation';
@@ -9,7 +9,7 @@ export const useEditPage = () => {
   const bookId = params.id as string;
 
   const { updateBookMutation } = useBookMutations();
-  const { data: book } = useBookSearchById(bookId);
+  const { data: book } = useGetBookByID(bookId);
   const apiLoading = useDebounce(updateBookMutation.isPending, 300);
 
   const handleSubmitUpdate = async (formData: FormData) => {
