a838694eb98ee0810785629a8329d01a350c686c
nguyentuankiet <ntk28042002@gmail.com>
2025-05-28 21:06:15 +0700

    fix: pass storeIdParam to UserStoreForm component for proper data handling

--- src/app/(protected)/(dashboard)/(admin)/userstores/[id]/rent/page.tsx
+++ src/app/(protected)/(dashboard)/(admin)/userstores/[id]/rent/page.tsx
@@ -17,7 +17,7 @@ const UserStoreRentPage = () => {
     <div className='container mx-auto p-4 md:px-24'>
       <h1 className='text-2xl font-bold'>Đơn đăng ký</h1>
       <div className='mt-4 rounded-lg border bg-background p-16 shadow-md'>
-        <UserStoreForm />
+        <UserStoreForm storeIdParam={params.id as string} />
       </div>
     </div>
   );
